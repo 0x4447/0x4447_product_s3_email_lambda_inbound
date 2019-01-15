998e6105434dbd323783cdc657d8dcfc95ba0098
David Gatti <github@gatti.pl>
2019-01-15 12:03:24 +0100

    Partially tested code.

--- index.js
+++ index.js
@@ -1,22 +1,195 @@
+let AWS = requre('aws-sdk');
+
 //
 //	This Lambda will filter all the incoming emails based on their From and To
 //	field.
 //
 exports.handler = async (event) => {
 
-	let from = event.Records[0].ses.mail.commonHeaders.from[0];
-	let to = event.Records[0].ses.mail.commonHeaders.to[0];
-	let date = event.Records[0].ses.mail.commonHeaders.date;
-	let message_id = event.Records[0].ses.mail.messageId;
+	//
+	//	1.	This JS object will contain all the data within the chain.
+	//
+	let container = {
+		from: event.Records[0].ses.mail.commonHeaders.from[0],
+		to: event.Records[0].ses.mail.commonHeaders.to[0],
+		subject: event.Records[0].ses.mail.commonHeaders.subject,
+		date: event.Records[0].ses.mail.commonHeaders.date,
+		message_id: event.Records[0].ses.mail.messageId
+	}
 
-	console.log("From: ", from);
-	console.log("to: ", to);
-	console.log("date: ", date);
-	console.log("message_id: ", message_id);
+	console.log(container);
 
 	//
-	//	->	Return a positive response
+	//	->	Start the chain.
 	//
-	return true;
+	extract_data(container)
+		.then(function(container) {
+
+			return true;
+
+		}).catch(function(error) {
+
+			console.error(error);
+
+			return false;
+
+		});
 
 };
+
+//	 _____    _____     ____    __  __   _____    _____   ______    _____
+//	|  __ \  |  __ \   / __ \  |  \/  | |_   _|  / ____| |  ____|  / ____|
+//	| |__) | | |__) | | |  | | | \  / |   | |   | (___   | |__    | (___
+//	|  ___/  |  _  /  | |  | | | |\/| |   | |    \___ \  |  __|    \___ \
+//	| |      | | \ \  | |__| | | |  | |  _| |_   ____) | | |____   ____) |
+//	|_|      |_|  \_\  \____/  |_|  |_| |_____| |_____/  |______| |_____/
+//
+
+//
+//	Extract all the data necessary to organize the incoming emails.
+//
+function extract_data(container)
+{
+	return new Promise(function(resolve, reject) {
+
+		//
+		//	1.	Extract all the information
+		//
+		let tmp_to = container.to.split('@');
+		let tmp_from = 	container
+						.from.match(/[^< ]+(?=>)/g)[0]
+						.split('@');
+
+		//
+		//	2.	Get the domain name of the receiving end, so we can group
+		//		emails by all the domain that were added to SES.
+		//
+		let to_domain = tmp_to[1];
+
+		//
+		//	3.	Get the email name where the email is directed to.
+		//
+		let user_name = tmp_to[0];
+
+		//
+		//	4.	Based on the email name, we replace all the + characters, that
+		//		can be used to organize ones on-line accounts in to /, this way
+		//		we can build a S3 patch which will automatically organize
+		//		all the email in structured folder.
+		//
+		let to_path = user_name.replace(/\+/g, "/");
+
+		//
+		//	5.	Get the domain name of the email which in our case will
+		//		become the company name.
+		//
+		let company_name = tmp_from[1];
+
+		//
+		//	6.	Get the name of who sent us the email.
+		//
+		let company_account = tmp_from[0];
+
+		//
+		//	7.	Create the path where the email needs to be moved
+		//		so it is properly organized.
+		//
+		let path = 	to_domain
+					+ "/" +
+					to_path
+					+ "/" +
+					company_name
+					+ "/" +
+					company_account;
+
+		console.log(path);
+
+		//
+		//	8.	Save the path for the next promise.
+		//
+		container.path = path;
+
+		//
+		//	->	Move to the next chain.
+		//
+		return resolve(container);
+
+	});
+}
+
+//
+//	Copy the email to a new location.
+//
+function copy_the_email(container)
+{
+	return new Promise(function(resolve, reject) {
+
+		//
+		//	1.	Set the query.
+		//
+		let params = {
+			Bucket: process.env.BUCKET,
+			CopySource: "/_inbound/" + container.message_id,
+			Key: container.path + "/" + container.subject
+		};
+
+		//
+		//	->	Execute the query.
+		//
+		s3.copyObject(params, function(error, data) {
+
+			//
+			//	1.	Check for internal errors.
+			//
+			if(error)
+			{
+				return reject(error);
+			}
+
+			//
+			//	->	Move to the next chain.
+			//
+			return resolve(container);
+
+		});
+
+	});
+}
+
+//
+//	Delete the original message.
+//
+function delete_the_email(container)
+{
+	return new Promise(function(resolve, reject) {
+
+		//
+		//	1.	Set the query.
+		//
+		let params = {
+			Bucket: process.env.BUCKET,
+			Key: "/_inbound/" + container.message_id
+		};
+
+		//
+		//	->	Execute the query.
+		//
+		s3.deleteObject(params, function(error, data) {
+
+			//
+			//	1.	Check for internal errors.
+			//
+			if(error)
+			{
+				return reject(error);
+			}
+
+			//
+			//	->	Move to the next chain.
+			//
+			return resolve(container);
+
+		});
+
+	});
+}
